2591df199bd7f97c2e0329fff5506af65e115660
pli9 <pli@westswan.com>
2025-10-23 23:15:13 +0800

    Initial Commit

---
+++ R/sqlDT.R
@@ -0,0 +1,103 @@
+#' Show the summary proportion in a widget
+#'
+#' A `sqlDT` displays a single statistic derived from a linked table, where
+#' the numerator and denominator columns are specific. This is best used with
+#' the `crosstalk`package, to allow dynamic filtering of the underlying data.
+#'
+#' @param data A data frame or data table containing the data to summarize,
+#' with at least two numeric columns: one for the numerator and one for the
+#' denominator. Normally, this should be an instance of [crosstalk::SharedData].
+#' @param query A SQL query string to filter the data before calculating the statistic.
+#' The `?` placeholder will be replaced with the data specified in the function.
+#' For example: "SELECT * FROM ?"
+#' @param width The width of the widget (optional).
+#' @param height The height of the widget (optional).
+#' @param elementId An optional element ID for the widget.
+#'
+#' @import crosstalk
+#' @import htmlwidgets
+#'
+#' @export
+#'
+#' @examples
+#' library(sqlDT)
+#' data_iris <- iris
+#' names(data_iris) <- c("SepalLength", "SepalWidth", "PetalLength", "PetalWidth", "Species")
+#' sqlDT(data_iris, query = "SELECT Species, round(sum(SepalLength)/count(*), 2) as Average_Sepal_Length FROM ? GROUP BY Species")
+#'
+sqlDT <- function(data, query, width = '100%', height = '100%', elementId = NULL) {
+
+  if (is.SharedData(data)) {
+    # Using Crosstalk
+    key <- data$key()
+    group <- data$groupName()
+    data <- data$origData()
+  } else {
+    # Not using Crosstalk
+    key <- NULL
+    group <- NULL
+  }
+
+  # Add checks for correct SQL query (i.e. "FROM ?")
+  if (!grepl("FROM \\?", query, ignore.case = TRUE)) {
+    stop("The SQL query must contain 'FROM ?' to indicate the data source.")
+  }
+
+  # Add check for column names in data meeting SQL requirements
+  if (any(grepl("\\s", names(data)))) {
+    stop("Column names in the data must not contain spaces.")
+  }
+  # Add check for column names for any non alphanumeric (except underscore)
+  if (any(!grepl("^[A-Za-z0-9_]+$", names(data)))) {
+    stop("Column names in the data must be alphanumeric or underscores only.")
+  }
+
+  # forward options using x
+  x <- list(
+    data = data,
+    settings = list(
+      query = query,
+      crosstalk_key = key,
+      crosstalk_group = group
+    )
+  )
+
+  # create widget
+  htmlwidgets::createWidget(
+    name = 'sqlDT',
+    x,
+    width = width,
+    height = height,
+    dependencies = crosstalk::crosstalkLibs(),
+    package = 'sqlDT',
+    elementId = elementId
+  )
+}
+
+#' Shiny bindings for sqlDT
+#'
+#' Output and render functions for using sqlDT within Shiny
+#' applications and interactive Rmd documents.
+#'
+#' @param outputId output variable to read from
+#' @param width,height Must be a valid CSS unit (like \code{'100\%'},
+#'   \code{'400px'}, \code{'auto'}) or a number, which will be coerced to a
+#'   string and have \code{'px'} appended.
+#' @param expr An expression that generates a sqlDT
+#' @param env The environment in which to evaluate \code{expr}.
+#' @param quoted Is \code{expr} a quoted expression (with \code{quote()})? This
+#'   is useful if you want to save an expression in a variable.
+#'
+#' @name sqlDT-shiny
+#'
+#' @export
+sqlDTOutput <- function(outputId, width = '100%', height = '400px'){
+  htmlwidgets::shinyWidgetOutput(outputId, 'sqlDT', width, height, package = 'sqlDT')
+}
+
+#' @rdname sqlDT-shiny
+#' @export
+rendersqlDT <- function(expr, env = parent.frame(), quoted = FALSE) {
+  if (!quoted) { expr <- substitute(expr) } # force quoted
+  htmlwidgets::shinyRenderWidget(expr, sqlDTOutput, env, quoted = TRUE)
+}
---
+++ inst/htmlwidgets/sqlDT.js
@@ -0,0 +1,102 @@
+HTMLWidgets.widget({
+
+  name: 'sqlDT',
+
+  type: 'output',
+
+  factory: function(el, width, height) {
+
+    // Filter obj, returning a new obj containing only
+    // values with keys in keys.
+    var filterKeys = function(obj, keys) {
+      var result = {};
+      keys.forEach(function(k) {
+        if (obj.hasOwnProperty(k))
+          result[k]=obj[k];});
+      return result;
+    };
+
+    return {
+
+      renderValue: function(x) {
+
+        // Transpose the data from column-oriented to row-oriented
+        var data = x.data;
+        var data_keys = Object.keys(data);
+        var crosstalk_keys = x.settings.crosstalk_key;
+        var transpose_data = {};
+        for (var i=0; i<crosstalk_keys.length; i++) {
+          transpose_data[crosstalk_keys[i]] = {};
+          for (var j=0; j<data_keys.length; j++) {
+            transpose_data[crosstalk_keys[i]][data_keys[j]] = data[data_keys[j]][i];
+          }          
+        }
+
+        // Update the display to show the values in data
+        var update = function(data) {
+          // Replace the keys in data with serial numbers
+          var new_data = [];
+          var data_keys = Object.keys(data);
+          for (var i=0; i<data_keys.length; i++) {
+            new_data[i] = data[data_keys[i]];
+          }
+          var result = alasql(x.settings.query, [new_data]);
+
+          // Clear the existing contents of el and add a table element
+          $(el).html('<table id="dataTable" class="display" style="width:100%"></table>');
+
+          // Create an array for the columns definition for DataTables for the result table
+          var columns = [];
+          if (result.length > 0) {
+            var result_keys = Object.keys(result[0]);
+            for (i=0; i<result_keys.length; i++) {
+              columns.push({data: result_keys[i], title: result_keys[i]});
+            }
+          }
+
+          $('#dataTable').DataTable({
+            data: result,
+            columns: columns,
+            pageLength: 500,
+            paging: false,
+            searching: true,
+            ordering: true,
+            info: false
+          });
+          
+        };
+
+        // Set up to receive crosstalk filter and selection events
+        var ct_filter = new crosstalk.FilterHandle();
+        ct_filter.setGroup(x.settings.crosstalk_group);
+        ct_filter.on("change", function(e) {
+          if (e.value) {
+            update(filterKeys(transpose_data, e.value));
+          } else {
+            update(transpose_data);
+          }
+        });
+
+        var ct_sel = new crosstalk.SelectionHandle();
+        ct_sel.setGroup(x.settings.crosstalk_group);
+        ct_sel.on("change", function(e) {
+          if (e.value && e.value.length) {
+            update(filterKeys(transpose_data, e.value));
+          } else {
+            update(transpose_data);
+          }
+        });
+
+        update(transpose_data);
+
+      },
+
+      resize: function(width, height) {
+
+        // TODO: code to re-render the widget with a new size
+
+      }
+
+    };
+  }
+});
